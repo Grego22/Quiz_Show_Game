0f475d5828bda4097b9eaf93409c658326c74499
Greg Garville Jr <gjgarvil@gmail.com>
2017-09-02 00:04:47 -0400

    updated routes.db

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
      get '/signup' => 'users#new'
      post '/users' => 'users#create'
 
-     get '/login' => 'session#new'
-     post '/login' => 'session#create'
-     get '/logout' => 'session#destroy'
+     get '/login' => 'sessions#new'
+     post '/login' => 'sessions#create'
+     get '/logout' => 'sessions#destroy'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
