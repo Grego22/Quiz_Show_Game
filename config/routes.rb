f3ba56dee713f97dc06f4827e98b6faa2916aa57
Greg Garville Jr <gjgarvil@gmail.com>
2017-09-01 23:52:03 -0400

    destroyed user and generated users

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
      get '/signup' => 'users#new'
      post '/users' => 'users#create'
 
+     get '/login' => 'session#new'
+     post '/login' => 'session#create'
+     get '/logout' => 'session#destroy'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
