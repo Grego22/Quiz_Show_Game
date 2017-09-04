8095ef43e9b32e53135d0600febe81d7d362065d
Greg Garville Jr <gjgarvil@gmail.com>
2017-09-04 14:30:32 -0400

    added admin role/seeding

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
 has_secure_password
+def admin?
+  self.role == 'admin'
+end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+mateo = User.create(first_name: 'Mateo', last_name: 'Lazo', email: 'mateo@email.com', password: 'Mateo1', password_confirmation: 'Mateo1', role: 'editor')
+julian = User.create(first_name: 'Julian', last_name: 'Jones', email: 'julian@email.com', password: 'Julian1', password_confirmation: 'Julian1')
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
